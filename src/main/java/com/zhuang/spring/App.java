2071c85939dc40f9919a5cca9366b457da71565f
weibin268 <binweizhuang@hotmail.com>
2017-01-04 01:07:09 +0800

    aa

--- src/main/java/com/zhuang/spring/App.java
+++ src/main/java/com/zhuang/spring/App.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.zhuang.spring.test.Father;
+import com.zhuang.spring.test.TestService;
 import com.zhuang.spring.utils.ApplicationContextUtil;
 
 /**
@@ -14,15 +15,13 @@ import com.zhuang.spring.utils.ApplicationContextUtil;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
     	Father fa = ApplicationContextUtil.GetApplicationContext().getBean("father",Father.class);
     	fa.driveCar();
 
-    	JdbcTemplate jdbcTemplate = ApplicationContextUtil.GetApplicationContext().getBean("jdbcTemplate",JdbcTemplate.class);
+    	TestService testService = ApplicationContextUtil.GetApplicationContext().getBean("testService",TestService.class);
     	
-    	List<Map<String,Object>> map = jdbcTemplate.queryForList("select * from sys_autocode");
-    
-    	System.out.println(map);
+    	testService.delTest();
     }
 }
---
+++ src/main/java/com/zhuang/spring/test/TestService.java
@@ -0,0 +1,30 @@
+package com.zhuang.spring.test;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class TestService{
+
+	private JdbcTemplate jdbcTemplate;
+
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	public void delTest() {
+
+		Integer integer = jdbcTemplate.queryForObject("select count(*) from sys_autocodedetail",Integer.class);
+	
+		System.out.println(integer);
+			
+		jdbcTemplate.execute("delete from sys_autocodedetail");
+		
+		int i=1/0;
+	
+	}
+	
+	
+}
